af792f5270ee3f5b4bb2a91b34554924e677fd98
yaira120 <yairnad247@gmail.com>
2023-02-26 16:48:03 +0200

    bug fixes

--- src/Components/Services/globals.ts
+++ src/Components/Services/globals.ts
@@ -14,10 +14,10 @@ class DevelopmentGlobals extends Globals{
 class ProductionGlobals extends Globals{
     public urls = {
        
-         company: "www.aws.com/Website/welcome",
-         homePage:"http://localhost:8080/api/home",
-         admin:"www.aws.com/mkk",
-        customer:"www.aws.com/mkk"
+        company: "https://couponsbackendcloud-production.up.railway.app/api/company",
+        homePage:"https://couponsbackendcloud-production.up.railway.app/api/home",
+        admin:"https://couponsbackendcloud-production.up.railway.app/api/admin",
+        customer:"https://couponsbackendcloud-production.up.railway.app/api/customers"
 
 
 
